50e0ebbe66c74ac4f8a6c080b8ce22d55f987f75
Морозов Андрей Владимирович <avmorozov2001@gmail.com>
2025-02-08 03:44:14 +0300

    init commit

---
+++ src/main/java/ru/snptech/ritualbitrixbot/RitualBitrixBotApplication.java
@@ -0,0 +1,13 @@
+package ru.snptech.ritualbitrixbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RitualBitrixBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RitualBitrixBotApplication.class, args);
+    }
+
+}
